bc54d076a0ed3222911215e657b495636802f0ad
T1chu <meditizi49@gmail.com>
2024-06-28 16:39:28 -0300

    Septimo commit

--- logoman.py
+++ logoman.py
@@ -2,7 +2,7 @@ import pygame
 import sys
 import random
 import time
-
+from logos import *
 
 pygame.init()
 
@@ -16,92 +16,12 @@ ROJO = (255, 0, 0)
 
 fuente = pygame.font.Font(None, 36)
 
-logos_correctos = [
-    {"nombre": "Apple", "imagen": pygame.image.load("logos_correctos\logo_correcto_apple.png"), "incorrectas": [
-        pygame.image.load("logos_incorrectos\logo_incorrecto_apple_1.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_apple_2.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_apple_3.png")
-    ]},
-    {"nombre": "Pepsi", "imagen": pygame.image.load("logos_correctos\logo_correcto_pepsi.png"), "incorrectas": [
-        pygame.image.load("logos_incorrectos\logo_incorrecto_pepsi_1.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_pepsi_2.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_pepsi_3.png")
-    ]},
-    {"nombre": "Youtube", "imagen": pygame.image.load("logos_correctos\logo_correcto_youtube.png"), "incorrectas": [
-        pygame.image.load("logos_incorrectos\logo_incorrecto_youtube_1.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_youtube_2.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_youtube_3.png")
-    ]},
-    {"nombre": "Mercedes", "imagen": pygame.image.load("logos_correctos\logo_correcto_mercedes.png"), "incorrectas": [
-        pygame.image.load("logos_incorrectos\logo_incorrecto_mercedes_1.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_mercedes_2.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_mercedes_3.png")
-    ]},
-    {"nombre": "Mcdonald", "imagen": pygame.image.load("logos_correctos\logo_correcto_mcdonalds.png"), "incorrectas": [
-        pygame.image.load("logos_incorrectos\logo_incorrecto_mcdonalds_1.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_mcdonalds_2.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_mcdonalds_3.png")
-    ]},
-    {"nombre": "Nike", "imagen": pygame.image.load("logos_correctos\logo_correcto_nike.png"), "incorrectas": [
-        pygame.image.load("logos_incorrectos\logo_incorrrecto_nike_1.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrrecto_nike_2.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrrecto_nike_3.png")
-    ]},
-    {"nombre": "Toyota", "imagen": pygame.image.load("logos_correctos\logo_correcto_toyota.png"), "incorrectas": [
-        pygame.image.load("logos_incorrectos\logo_incorrecto_toyota_1.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_toyota_2.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_toyota_3.png")
-    ]},
-    {"nombre": "LG", "imagen": pygame.image.load("logos_correctos\logo_correcto_lg.png"), "incorrectas": [
-        pygame.image.load("logos_incorrectos\logo_incorrecto_lg_1.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_lg_2.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_lg_3.png")
-    ]},
-    {"nombre": "Superman", "imagen": pygame.image.load("logos_correctos\logo_correcto_superman.png"), "incorrectas": [
-        pygame.image.load("logos_incorrectos\logo_incorrecto_superman_1.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_superman_2.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_superman_3.png")
-    ]},
-    {"nombre": "Lego", "imagen": pygame.image.load("logos_correctos\logo_correcto_lego.png"), "incorrectas": [
-        pygame.image.load("logos_incorrectos\logo_incorrecto_lego_1.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_lego_2.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_lego_3.png")
-    ]},
-    {"nombre": "Snickers", "imagen": pygame.image.load("logos_correctos\logo_correcto_snickers.png"), "incorrectas": [
-        pygame.image.load("logos_incorrectos\logo_incorrecto_snickers_1.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_snickers_2.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_snickers_3.png")
-    ]},
-    {"nombre": "Adidas", "imagen": pygame.image.load("logos_correctos\logo_correcto_adidas.png"), "incorrectas": [
-        pygame.image.load("logos_incorrectos\logo_incorrecto_adidas_1.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_adidas_2.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_adidas_3.png")
-    ]},
-    {"nombre": "Microsoft", "imagen": pygame.image.load("logos_correctos\logo_correcto_microsoft.png"), "incorrectas": [
-        pygame.image.load("logos_incorrectos\logo_incorrecto_microsoft_1.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_microsoft_2.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_microsoft_3.png")
-    ]},
-    {"nombre": "Kinder", "imagen": pygame.image.load("logos_correctos\logo_correcto_kinder.png"), "incorrectas": [
-        pygame.image.load("logos_incorrectos\logo_incorrecto_kinder_1.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_kinder_2.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_kinder_3.png")
-    ]},
-    {"nombre": "Nokia", "imagen": pygame.image.load("logos_correctos\logo_correcto_nokia.png"), "incorrectas": [
-        pygame.image.load("logos_incorrectos\logo_incorrecto_nokia_1.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_nokia_2.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_nokia_3.png")
-    ]}
-]
-
-
-
-# Función para mostrar un mensaje en la pantalla
+# mostrar un mensaje en la pantalla
 def mostrar_mensaje(texto, posicion):
     texto_superficie = fuente.render(texto, True, NEGRO)
     pantalla.blit(texto_superficie, posicion)
 
-# Función para seleccionar un logo correcto y opciones incorrectas
+# seleccionar un logo correcto y opciones incorrectas
 def seleccionar_logo_y_opciones(ya_usados):
     disponibles = [logo for logo in logos_correctos if logo["nombre"] not in ya_usados]
     if not disponibles:
@@ -120,15 +40,13 @@ rounds = 0
 tiempos_respuestas = []
 comodines = {"Next": 1, "Half": 1, "Reload": 1}
 ya_usados = []  # Lista para llevar el seguimiento de los logos
-mostrar_alerta = False
-tiempo_alerta = 0
 
-# Función para mostrar las vidas
+# mostrar las vidas
 def mostrar_vidas(vidas):
     for i in range(vidas):
         pygame.draw.circle(pantalla, ROJO, (30 + i * 30, 30), 10)
 
-# Función para usar comodín Half
+# usar comodín Half
 def usar_half(opciones, logo_correcto):
     incorrectas = [op for op in opciones if op != logo_correcto["imagen"]]
     opciones_mostradas = [logo_correcto["imagen"], random.choice(incorrectas)]
@@ -209,7 +127,7 @@ while ejecutando and rounds < 15:
         
         pygame.display.update()
 
-    # pASA A LA SIGUIENTE RONDA Y MODIFICA TIMEPO
+    # PASA A LA SIGUIENTE RONDA Y MODIFICA TIEMPO
     if siguiente_ronda:
         rounds += 1
         tiempo_respuesta = time.time() - tiempo_inicio
---
+++ logos.py
@@ -0,0 +1,79 @@
+import pygame
+
+logos_correctos = [
+    {"nombre": "Apple", "imagen": pygame.image.load("logos_correctos\logo_correcto_apple.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_apple_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_apple_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_apple_3.png")
+    ]},
+    {"nombre": "Pepsi", "imagen": pygame.image.load("logos_correctos\logo_correcto_pepsi.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_pepsi_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_pepsi_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_pepsi_3.png")
+    ]},
+    {"nombre": "Youtube", "imagen": pygame.image.load("logos_correctos\logo_correcto_youtube.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_youtube_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_youtube_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_youtube_3.png")
+    ]},
+    {"nombre": "Mercedes", "imagen": pygame.image.load("logos_correctos\logo_correcto_mercedes.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_mercedes_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_mercedes_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_mercedes_3.png")
+    ]},
+    {"nombre": "Mcdonald", "imagen": pygame.image.load("logos_correctos\logo_correcto_mcdonalds.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_mcdonalds_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_mcdonalds_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_mcdonalds_3.png")
+    ]},
+    {"nombre": "Nike", "imagen": pygame.image.load("logos_correctos\logo_correcto_nike.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrrecto_nike_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrrecto_nike_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrrecto_nike_3.png")
+    ]},
+    {"nombre": "Toyota", "imagen": pygame.image.load("logos_correctos\logo_correcto_toyota.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_toyota_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_toyota_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_toyota_3.png")
+    ]},
+    {"nombre": "LG", "imagen": pygame.image.load("logos_correctos\logo_correcto_lg.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_lg_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_lg_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_lg_3.png")
+    ]},
+    {"nombre": "Superman", "imagen": pygame.image.load("logos_correctos\logo_correcto_superman.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_superman_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_superman_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_superman_3.png")
+    ]},
+    {"nombre": "Lego", "imagen": pygame.image.load("logos_correctos\logo_correcto_lego.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_lego_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_lego_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_lego_3.png")
+    ]},
+    {"nombre": "Snickers", "imagen": pygame.image.load("logos_correctos\logo_correcto_snickers.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_snickers_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_snickers_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_snickers_3.png")
+    ]},
+    {"nombre": "Adidas", "imagen": pygame.image.load("logos_correctos\logo_correcto_adidas.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_adidas_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_adidas_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_adidas_3.png")
+    ]},
+    {"nombre": "Microsoft", "imagen": pygame.image.load("logos_correctos\logo_correcto_microsoft.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_microsoft_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_microsoft_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_microsoft_3.png")
+    ]},
+    {"nombre": "Kinder", "imagen": pygame.image.load("logos_correctos\logo_correcto_kinder.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_kinder_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_kinder_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_kinder_3.png")
+    ]},
+    {"nombre": "Nokia", "imagen": pygame.image.load("logos_correctos\logo_correcto_nokia.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_nokia_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_nokia_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_nokia_3.png")
+    ]}
+]
