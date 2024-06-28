b120d609f854b11730780fccbe22eefb7e510834
T1chu <meditizi49@gmail.com>
2024-06-27 21:07:53 -0300

    Tercer commit

--- logoman.py
+++ logoman.py
@@ -46,20 +46,45 @@ logos_correctos = [
         pygame.image.load("logos_incorrectos\logo_incorrecto_mcdonalds_2.png"),
         pygame.image.load("logos_incorrectos\logo_incorrecto_mcdonalds_3.png")
     ]},
-    {"nombre": "Pepsi", "imagen": pygame.image.load("logos_correctos\logo_correcto_pepsi.png"), "incorrectas": [
-        pygame.image.load("logos_incorrectos\logo_incorrecto_pepsi_1.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_pepsi_2.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_pepsi_3.png")
+    {"nombre": "Nike", "imagen": pygame.image.load("logos_correctos\logo_correcto_nike.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrrecto_nike_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrrecto_nike_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrrecto_nike_3.png")
     ]},
-    {"nombre": "Youtube", "imagen": pygame.image.load("logos_correctos\logo_correcto_youtube.png"), "incorrectas": [
-        pygame.image.load("logos_incorrectos\logo_incorrecto_youtube_1.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_youtube_2.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_youtube_3.png")
+    {"nombre": "Toyota", "imagen": pygame.image.load("logos_correctos\logo_correcto_toyota.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_toyota_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_toyota_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_toyota_3.png")
     ]},
-    {"nombre": "Mercedes", "imagen": pygame.image.load("logos_correctos\logo_correcto_mercedes.png"), "incorrectas": [
-        pygame.image.load("logos_incorrectos\logo_incorrecto_mercedes_1.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_mercedes_2.png"),
-        pygame.image.load("logos_incorrectos\logo_incorrecto_mercedes_3.png")
+    {"nombre": "LG", "imagen": pygame.image.load("logos_correctos\logo_correcto_lg.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_lg_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_lg_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_lg_3.png")
+    ]},
+    {"nombre": "Superman", "imagen": pygame.image.load("logos_correctos\logo_correcto_superman.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_superman_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_superman_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_superman_3.png")
+    ]},
+    {"nombre": "Lego", "imagen": pygame.image.load("logos_correctos\logo_correcto_lego.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_lego_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_lego_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_lego_1.png")
+    ]},
+    {"nombre": "Snickers", "imagen": pygame.image.load("logos_correctos\logo_correcto_snickers.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_snickers_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_snickers_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_snickers_3.png")
+    ]},
+    {"nombre": "LG", "imagen": pygame.image.load("logos_correctos\logo_correcto_lg.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_lg_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_lg_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_lg_3.png")
+    ]},
+    {"nombre": "LG", "imagen": pygame.image.load("logos_correctos\logo_correcto_lg.png"), "incorrectas": [
+        pygame.image.load("logos_incorrectos\logo_incorrecto_lg_1.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_lg_2.png"),
+        pygame.image.load("logos_incorrectos\logo_incorrecto_lg_3.png")
     ]}
     # Añade más logos correctos con sus incorrectos según sea necesario
 ]
