67653ee61959bf76bfb5a5a418b66e33b06e4e99
T1chu <meditizi49@gmail.com>
2024-07-03 16:46:50 -0300

    decimo tercer commit

--- logoman.py
+++ logoman.py
@@ -10,45 +10,42 @@ TAMAÑO_PANTALLA = (1200, 700)
 pantalla = pygame.display.set_mode(TAMAÑO_PANTALLA)
 pygame.display.set_caption("Logoman")
 
-# Definir colores
 BLANCO = (255, 255, 255)
 NEGRO = (0, 0, 0)
 ROJO = (255, 0, 0)
 GRIS = (128, 136, 142)
 
-# Cargar imagen de fondo
-fondo = pygame.image.load("fondoymusica/paisaje-nevado-en-el-bosque-arte-digital_3840x2160_xtrafondos.com.jpg").convert()
+fondo = pygame.image.load("fondoymusica\images.jpg").convert()
 fondo = pygame.transform.scale(fondo, TAMAÑO_PANTALLA)
 
-# Cargar sonido de fondo
-pygame.mixer.music.load("fondoymusica/Y2meta.app - Martin Garrix - Animals (Official Lyrics Video) (128 kbps).mp3")
-pygame.mixer.music.set_volume(0.1)  # Volumen inicial
-pygame.mixer.music.play(-1)  # Reproducir en bucle
-
-# Cargar icono del juego
 icono = pygame.image.load("logos_correctos/icono_izquierda.png")
 pygame.display.set_icon(icono)
 
-# Fuente para el texto
+logo = pygame.image.load("logos_correctos\logo.png")
+logo = pygame.transform.scale(logo, (320,170))
+
 fuente = pygame.font.Font(None, 36)
 
-# Variables del juego
+pygame.mixer.music.load("fondoymusica/Y2meta.app - Martin Garrix - Animals (Official Lyrics Video) (128 kbps).mp3")
+pygame.mixer.music.set_volume(0.1)
+pygame.mixer.music.play(-1)
+
 vidas = 5
 monedas = 0
 rounds = 0
 tiempos_respuestas = []
 comodines = {"Next": 1, "Half": 1, "Reload": 1}
-ya_usados = []  # Lista para llevar el seguimiento de los logos
+ya_usados = []
 promedio_tiempo = 0
-record_monedas = 0  # Variable para almacenar el record de monedas
+record_monedas = 0
 
 # Variables para el control de volumen
 volumen_encendido = True
 icono_volumen_on = pygame.image.load("fondoymusica/volumenon.png")
-icono_volumen_on = pygame.transform.scale(icono_volumen_on, (50,50))
+icono_volumen_on = pygame.transform.scale(icono_volumen_on, (50, 50))
 icono_volumen_off = pygame.image.load("fondoymusica/images.png")
-icono_volumen_off = pygame.transform.scale(icono_volumen_off, (50,50))
-icono_volumen_actual = icono_volumen_on  # Inicialmente encendido
+icono_volumen_off = pygame.transform.scale(icono_volumen_off, (50, 50))
+icono_volumen_actual = icono_volumen_on
 
 def mostrar_mensaje(texto, posicion):
     texto_superficie = fuente.render(texto, True, NEGRO)
@@ -57,7 +54,7 @@ def mostrar_mensaje(texto, posicion):
 def seleccionar_logo_y_opciones(ya_usados):
     disponibles = [logo for logo in logos_correctos if logo["nombre"] not in ya_usados]
     if not disponibles:
-        return None, None  # No hay más logos
+        return None, None
     logo_correcto = random.choice(disponibles)
     opciones = random.sample(logo_correcto["incorrectas"], 3)
     opciones.append(logo_correcto["imagen"])
@@ -76,36 +73,31 @@ def usar_half(opciones, logo_correcto):
     return opciones_mostradas
 
 def pantalla_inicial():
-    pantalla.blit(fondo, (0, 0))  # Dibujar fondo de pantalla
+    pantalla.blit(fondo, (0, 0))
 
-    mostrar_mensaje("Bienvenido a Logoman", (450, 90))
+    pantalla.blit(logo, (470, 30))
 
-    boton_jugar = pygame.Rect(500, 200, 180, 50)
+    boton_jugar = pygame.Rect(500, 200, 250, 50)
     boton_promedio = pygame.Rect(500, 300, 250, 50)
-    boton_coins = pygame.Rect(500, 400, 200, 50)
+    boton_coins = pygame.Rect(500, 400, 250, 50)
     boton_record = pygame.Rect(500, 500, 250, 50)
-    boton_volumen = pygame.Rect(30, 50, 200, 60)  
-    
-    pygame.draw.rect(pantalla, GRIS, boton_jugar)
-    pygame.draw.rect(pantalla, GRIS, boton_promedio)
-    pygame.draw.rect(pantalla, GRIS, boton_coins)
-    pygame.draw.rect(pantalla, GRIS, boton_record)
-    pygame.draw.rect(pantalla, BLANCO, boton_volumen)  # Dibujar botón de volumen
+    boton_volumen = pygame.Rect(30, 50, 200, 60)
 
-    mostrar_mensaje("Jugar", (550, 215))
+    pygame.draw.rect(pantalla, ROJO, boton_jugar, border_radius=10)
+    pygame.draw.rect(pantalla, ROJO, boton_promedio, border_radius=10)
+    pygame.draw.rect(pantalla, ROJO, boton_coins, border_radius=10)
+    pygame.draw.rect(pantalla, ROJO, boton_record, border_radius=10)
+    pygame.draw.rect(pantalla, BLANCO, boton_volumen, border_radius=10)
+
+    mostrar_mensaje("Jugar", (590, 215))
     mostrar_mensaje("Promedio de tiempo", (510, 315))
-    mostrar_mensaje("Coins", (570, 415))
+    mostrar_mensaje("Coins", (590, 415))
     mostrar_mensaje("Record de monedas", (510, 515))
 
-    # Dibujar ícono de volumen actual
     pantalla.blit(icono_volumen_actual, (150, 50))
-    
-    # Mostrar estado del volumen
+
     texto_volumen = "Music on" if volumen_encendido else "Music off"
-    if texto_volumen == "Music on":
-        mostrar_mensaje(texto_volumen, (30, 70))
-    else:
-        mostrar_mensaje(texto_volumen, (30, 70))
+    mostrar_mensaje(texto_volumen, (30, 70))
 
     pygame.display.flip()
     return boton_jugar, boton_promedio, boton_coins, boton_record, boton_volumen
@@ -114,10 +106,10 @@ def cambiar_volumen():
     global volumen_encendido, icono_volumen_actual
     volumen_encendido = not volumen_encendido
     if volumen_encendido:
-        pygame.mixer.music.set_volume(0.1)  # Volver a encender el volumen
+        pygame.mixer.music.set_volume(0.1)
         icono_volumen_actual = icono_volumen_on
     else:
-        pygame.mixer.music.set_volume(0.0)  # Apagar el volumen
+        pygame.mixer.music.set_volume(0.0)
         icono_volumen_actual = icono_volumen_off
 
 def jugar():
@@ -131,17 +123,17 @@ def jugar():
     ya_usados = []
 
     ejecutando = True
-    tiempo_inicio_partida = time.time()  # Iniciar tiempo de la partida
+    tiempo_inicio_partida = time.time()
 
     while ejecutando and rounds < 15:
         logo_correcto, opciones = seleccionar_logo_y_opciones(ya_usados)
-        if logo_correcto is None:  # No hay más logos
+        if logo_correcto is None:
             break
         nombre_empresa = logo_correcto["nombre"]
-        tiempo_inicio_ronda = time.time()  # Iniciar tiempo de la ronda
-        opciones_mostradas = opciones 
+        tiempo_inicio_ronda = time.time()
+        opciones_mostradas = opciones
         siguiente_ronda = False
-        
+
         while not siguiente_ronda and ejecutando:
             for evento in pygame.event.get():
                 if evento.type == pygame.QUIT:
@@ -165,7 +157,6 @@ def jugar():
                     elif botones_opciones[0].collidepoint(x, y) or botones_opciones[1].collidepoint(x, y) or botones_opciones[2].collidepoint(x, y) or botones_opciones[3].collidepoint(x, y):
                         monedas -= 10
                         vidas -= 1
-                        # No cambiar de ronda ni actualizar opciones, solo continuar en la misma
                     elif boton_next.collidepoint(x, y) and comodines["Next"] > 0:
                         comodines["Next"] -= 1
                         monedas += 20
@@ -176,26 +167,26 @@ def jugar():
                     elif boton_reload.collidepoint(x, y) and comodines["Reload"] > 0:
                         comodines["Reload"] -= 1
                         logo_correcto, opciones = seleccionar_logo_y_opciones(ya_usados)
-                        if logo_correcto is None:  
+                        if logo_correcto is None:
                             break
                         nombre_empresa = logo_correcto["nombre"]
                         opciones_mostradas = opciones
-                        tiempo_inicio_ronda = time.time() 
+                        tiempo_inicio_ronda = time.time()
 
             tiempo_transcurrido_ronda = time.time() - tiempo_inicio_ronda
-            if tiempo_transcurrido_ronda > 15:  
+            if tiempo_transcurrido_ronda > 30:
                 vidas -= 1
-                # No cambiar de ronda ni actualizar opciones, solo continuar en la misma
+                siguiente_ronda = True
 
             if vidas <= 0:
-                siguiente_ronda = True  # Forzar salida de la ronda si se quedan sin vidas
+                siguiente_ronda = True
 
             if not ejecutando or rounds >= 15 or vidas <= 0:
                 break
 
-            pantalla.blit(fondo, (0, 0))  
+            pantalla.blit(fondo, (0, 0))
             mostrar_mensaje(f"¿Cuál es el logo de {nombre_empresa}?", (400, 100))
-            
+
             botones_opciones = []
             for i, opcion in enumerate(opciones_mostradas):
                 separacion = 250
@@ -205,17 +196,22 @@ def jugar():
 
             mostrar_vidas(vidas)
 
-            boton_half = pygame.Rect(200, 550, 150, 50)
-            boton_next = pygame.Rect(400, 550, 150, 50)
-            boton_reload = pygame.Rect(600, 550, 150, 50)
+            boton_half = pygame.Rect(300, 550, 150, 50)
+            boton_next = pygame.Rect(500, 550, 150, 50)
+            boton_reload = pygame.Rect(700, 550, 150, 50)
+            boton_temporizador = pygame.Rect(1000, 20, 180, 50)
 
-            pygame.draw.rect(pantalla, ROJO, boton_next)
-            pygame.draw.rect(pantalla, ROJO, boton_half)
-            pygame.draw.rect(pantalla, ROJO, boton_reload)
+            pygame.draw.rect(pantalla, ROJO, boton_next, border_radius=10)
+            pygame.draw.rect(pantalla, ROJO, boton_half, border_radius=10)
+            pygame.draw.rect(pantalla, ROJO, boton_reload, border_radius=10)
+            pygame.draw.rect(pantalla, ROJO, boton_temporizador, border_radius=10)
 
-            mostrar_mensaje("Next", (440, 565))
-            mostrar_mensaje("Half", (230, 565))
-            mostrar_mensaje("Reload", (630, 565))
+            mostrar_mensaje("Next", (550, 565))
+            mostrar_mensaje("Half", (350, 565))
+            mostrar_mensaje("Reload", (730, 565))
+
+            tiempo_restante = max(0, 30 - int(tiempo_transcurrido_ronda))
+            mostrar_mensaje(f"Tiempo: {tiempo_restante} s", (1010, 35))
 
             pygame.display.update()
 
@@ -233,28 +229,27 @@ def jugar():
     if monedas > record_monedas:
         record_monedas = monedas
 
-    pantalla.blit(fondo, (0, 0))  # Dibujar fondo de pantalla
+    pantalla.blit(fondo, (0, 0))
     mostrar_mensaje("Juego terminado", (300, 250))
     mostrar_mensaje(f"Promedio de tiempo de respuesta: {promedio_tiempo:.2f} segundos", (200, 300))
     mostrar_mensaje(f"Total de monedas obtenidas: {monedas}", (200, 350))
     pygame.display.update()
     time.sleep(5)
 
-
 def mostrar_promedio():
-    pantalla.blit(fondo, (0, 0))  # Dibujar fondo de pantalla
+    pantalla.blit(fondo, (0, 0))
     mostrar_mensaje(f"Promedio de tiempo de respuesta: {promedio_tiempo:.2f} segundos", (400, 300))
     pygame.display.update()
     time.sleep(3)
 
 def mostrar_coins():
-    pantalla.blit(fondo, (0, 0))  # Dibujar fondo de pantalla
+    pantalla.blit(fondo, (0, 0))
     mostrar_mensaje(f"Total de monedas obtenidas: {monedas}", (400, 300))
     pygame.display.update()
     time.sleep(3)
 
 def mostrar_record():
-    pantalla.blit(fondo, (0, 0))  # Dibujar fondo de pantalla
+    pantalla.blit(fondo, (0, 0))
     mostrar_mensaje(f"Record de monedas: {record_monedas}", (400, 300))
     pygame.display.update()
     time.sleep(3)
@@ -286,4 +281,3 @@ while ejecutando:
 
 pygame.quit()
 sys.exit()
-
